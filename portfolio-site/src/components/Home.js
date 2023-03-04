899e2496546e32713e6ea3e06e3cec4d5b81f731
jtrinidad961 <trinidad961@gmail.com>
2023-03-03 22:17:27 -0700

    Prelim home page

--- portfolio-site/src/components/Home.js
+++ portfolio-site/src/components/Home.js
@@ -2,18 +2,23 @@ import React from "react";
 import headshot from "../assets/images/headshot.PNG"
 import "./Home.css"
 import MyCarousel from "./MyCarousel";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 const Home = () => (
     <div className="grid-container">
-        <img className="headshot" src={headshot} alt="Headshot" />
-        <p className="about-me">
-            My name is Jason. I chose to pursue a BS in Mechanical Engineering at Stanford ('20) because I
-            wanted to create useful products at scale. I am seeking a job as a
-            Software Engineer to crank up the scale even more! (After all, bits are cheaper than atoms!)
-            I hope the projects on this site show demonstrate my ability to learn quickly and my dedication to
-            spectacular results.
-        </p>
-        <MyCarousel className="carousel" />
+        <div className="about-container">
+            <img className="headshot" src={headshot} alt="Headshot" />
+            <p className="about-me">
+                My name is Jason. I'm a Software Engineer, amateur maker,
+                 and car enthusiast. Take a look at some of my projects!
+            </p>
+        </div>
+        <Container>
+            <Row className="justify-content-md-center">
+                <MyCarousel />
+            </Row>
+        </Container>
     </div>
 )
 
--- portfolio-site/src/components/MyCarousel.js
+++ portfolio-site/src/components/MyCarousel.js
@@ -1,44 +1,36 @@
-import Carousel from 'react-bootstrap/Carousel';
+import Carousel from "react-bootstrap/Carousel";
 import Mamba from "../assets/images/Mamba.JPG";
 import Reina from "../assets/images/Reina.jpg";
-import Productive from "../assets/images/Productive.png"
+import Productive from "../assets/images/Productive.png";
 
 const MyCarousel = () => (
-    <Carousel fade>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={Mamba}
-          alt="2019 Stanford Solar Car"
-        />
-        <Carousel.Caption>
-          <h3>2019 Stanford Solar Car</h3>
-          <p>Aerodynamic design</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={Reina}
-          alt="Reina"
-        />
-        <Carousel.Caption>
-          <h3>Reina</h3>
-          <p>A pen designed and built from scratch</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={Productive}
-          alt="Productive"
-        />
-        <Carousel.Caption style={{color: "black"}}>
-          <h3>Productive</h3>
-          <p>The ultimate productivity app</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
+  <Carousel className="w-50" fade>
+    <Carousel.Item>
+      <img className="d-block w-100" src={Productive} alt="Productive" />
+      <Carousel.Caption style={{ color: "black" }}>
+        <h3>Productive</h3>
+        <p>The ultimate productivity app</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src={Mamba}
+        alt="2019 Stanford Solar Car"
+      />
+      <Carousel.Caption>
+        <h3>2019 Stanford Solar Car</h3>
+        <p>Aerodynamic design</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img className="d-block w-100" src={Reina} alt="Reina" />
+      <Carousel.Caption>
+        <h3>Reina</h3>
+        <p>A pen designed and built from scratch</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
+);
 
 export default MyCarousel;

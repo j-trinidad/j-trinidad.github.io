89bdb62111562536da2c876eac635b3ac865bf3c
jtrinidad961 <trinidad961@gmail.com>
2023-03-07 15:36:04 -0700

    Cleanup homepage layout

--- portfolio-site/src/components/Home.js
+++ portfolio-site/src/components/Home.js
@@ -1,25 +1,25 @@
 import React from "react";
-import headshot from "../assets/images/headshot.PNG"
-import "./Home.css"
+import headshot from "../assets/images/headshot.PNG";
+import "./Home.css";
 import MyCarousel from "./MyCarousel";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 const Home = () => (
-    <div className="grid-container">
-        <div className="about-container">
-            <img className="headshot" src={headshot} alt="Headshot" />
-            <p className="about-me">
-                My name is Jason. I'm a software engineer, amateur maker,
-                 and car enthusiast. Take a look at some of my projects!
-            </p>
-        </div>
-        <Container>
-            <Row className="justify-content-md-center">
-                <MyCarousel />
-            </Row>
-        </Container>
+  <>
+    <div className="about-container">
+      <img className="headshot" src={headshot} alt="Headshot" />
+      <p className="about-me">
+        My name is Jason. I'm a software engineer, amateur maker, and car
+        enthusiast. Take a look at some of my projects!
+      </p>
     </div>
-)
+    <Container>
+      <Row className="justify-content-md-center">
+        <MyCarousel />
+      </Row>
+    </Container>
+  </>
+);
 
 export default Home;
